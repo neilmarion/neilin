a59f24a98ddcbfd844aac2384ce7a13a35326d48
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2013-10-13 10:59:44 +0800

    Fixed xy coordinates where the menu will appear

--- neilin.js
+++ neilin.js
@@ -1,36 +1,81 @@
-if(!window.Kolich){
-        Kolich = {};
-      }
-      
-      Kolich.Selector = {};
-      // getSelected() was borrowed from CodeToad at
-      // http://www.codetoad.com/javascript_get_selected_text.asp
-      Kolich.Selector.getSelected = function(){
-        var t = '';
-        if(window.getSelection){
-          t = window.getSelection();
-        }else if(document.getSelection){
-          t = document.getSelection();
-        }else if(document.selection){
-          t = document.selection.createRange().text;
-        }
-        return t;
-      }
-      
-      Kolich.Selector.mouseup = function(e){
-        var st = Kolich.Selector.getSelected();
-        if(st!=''){
-          $(".popup").show();
-          $(".popup").css({"left": e.pageX-40, "top": e.pageY-40});
-        }else{
-          $(".popup").hide();
-        }
-      }
-      
-      $(document).ready(function(){
-        highlightMenu = '<div class="popup highlight-menu highlight-menu-active"><div class="highlight-menu-inner"><ul class="highlight-menu-buttons"><li class="highlight-menu-button highlight-menu-notes"><button class="btn-highlight-menu" data-action="highlight"><span class="icons icon-facebook"></span></button></li><li class="highlight-menu-button highlight-menu-twitter"><button class="btn-highlight-menu" data-action="twitter"><span class="icons icon-twitter"></span></button></li></ul></div></div>';
+if(!window.Neilin){
+  Neilin = {};
+}
 
-        $("body").append(highlightMenu);
-        $(document).bind("mouseup", Kolich.Selector.mouseup);
-      });
+Neilin.Selector = {};
+// getSelected() was borrowed from CodeToad at
+// http://www.codetoad.com/javascript_get_selected_text.asp
+Neilin.Selector.getSelected = function(){
+  var t = '';
+  if(window.getSelection){
+    t = window.getSelection();
+  }else if(document.getSelection){
+    t = document.getSelection();
+  }else if(document.selection){
+    t = document.selection.createRange().text;
+  }
+  return t;
+}
+
+function computeXY(startX, startY, endX, endY){
+  var y; var addX;
+  
+  if(endY > startY){
+    y = startY; 
+  }else{
+    y = endY;
+  }
+
+  if(endX > startX){
+    addX = startX; 
+  }else{
+    addX = endX;
+  }
+
+
+  x = Math.abs(endX - startX) / 2;
+
+  //console.log(x+", "+y);
+
+  return [addX + x, y];
+}
+
+Neilin.Selector.mouseup = function(e){
+  var x = 0; var y = 1;
+  var xy;
+  window.endX = e.pageX;
+  window.endY = e.pageY;
+
+  console.log("startX: " + window.startX + " startY: " + window.startY + " endX: " + window.endX +  " endY: " + window.endY);
+
+  if(window.startX + window.startY != window.endX + window.endY) {
+    xy = computeXY(window.startX, window.startY, window.endX, window.endY);
+
+    var st = Neilin.Selector.getSelected();
+    if(st!=''){
+      $(".popup").show();
+      $(".popup").css({"left": xy[x]-40, "top": xy[y]-65});
+    }else{
+      $(".popup").hide();
+    }
+    window.endX = e.pageX
+    window.endY = e.pageY
+  }else{
+    $(".popup").hide();
+  }
+}
+
+Neilin.Selector.mousedown = function(e){
+  window.startX = e.pageX;
+  window.startY = e.pageY;
+}
+
+$(document).ready(function(){
+  highlightMenu = '<div class="popup highlight-menu highlight-menu-active"><div class="highlight-menu-inner"><ul class="highlight-menu-buttons"><li class="highlight-menu-button highlight-menu-notes"><button class="btn-highlight-menu" data-action="highlight"><span class="icons icon-facebook"></span></button></li><li class="highlight-menu-button highlight-menu-twitter"><button class="btn-highlight-menu" data-action="twitter"><span class="icons icon-twitter"></span></button></li></ul></div></div>';
+
+  $("body").append(highlightMenu);
+  $(document).bind("mouseup", Neilin.Selector.mouseup);
+  $(document).bind("mousedown", Neilin.Selector.mousedown);
+
+});
 
