df79a663fa2c85f314beb32f9838528481b06229
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2013-10-19 08:49:33 +0800

    Updated javascript file

--- neilin.js
+++ neilin.js
@@ -1,79 +1,78 @@
-if(!window.Neilin){
-  Neilin = {};
-}
+var TWITTER_SHARE_URL = 'https://twitter.com/intent/tweet?text='
+var GOOGLEPLUS_SHARE_URL = 'https://www.facebook.com/sharer/sharer.php?u='
+var FACEBOOKSHARE_URL = 'https://plus.google.com/share?url='
 
-Neilin.Selector = {};
+if(!window.Neilin){
+  Neilin = {}
+}
+Neilin.Selector = {}
 Neilin.Selector.getSelected = function(){
-  var t = '';
+  var t = ''
   if(window.getSelection){
-    t = window.getSelection();
+    t = window.getSelection()
   }else if(document.getSelection){
-    t = document.getSelection();
+    t = document.getSelection()
   }else if(document.selection){
-    t = document.selection.createRange().text;
+    t = document.selection.createRange().text
   }
-  return t;
+  return t
 }
 
 function formatMessage(message){
   if (message.length > 112){
-    message = message.substring(0, 112);
+    message = message.substring(0, 112)
     message = '"'+ message  +'..." ' 
   } else {
     message = '"'+ message  +'" ' 
   }
-  return message;
+  return message
 }
 
 function computeXY(){
-  range = Neilin.Selector.getSelected().getRangeAt(0);
-  range.collapse(false);
-  dummy = document.createElement("span");
-  range.insertNode(dummy);
-  rect = dummy.getBoundingClientRect();
-  endX = rect.left;
-  endY = rect.top;
-  dummy.parentNode.removeChild(dummy);
+  range = Neilin.Selector.getSelected().getRangeAt(0)
+  range.collapse(false)
+  dummy = document.createElement("span")
+  range.insertNode(dummy)
+  rect = dummy.getBoundingClientRect()
+  endX = rect.left
+  endY = rect.top
+  dummy.parentNode.removeChild(dummy)
 
-  range = Neilin.Selector.getSelected().getRangeAt(0); 
-  range.collapse(true);
-  rect = range.getClientRects()[0];
-  startX = rect.left;
-  startY = rect.top;
+  range = Neilin.Selector.getSelected().getRangeAt(0) 
+  range.collapse(true)
+  rect = range.getClientRects()[0]
+  startX = rect.left
+  startY = rect.top
   
-  y = endY > startY ? startY : endY;
-  addX = endX > startX ? startX : endX;
+  y = endY > startY ? startY : endY
+  addX = endX > startX ? startX : endX
 
-  x = Math.abs(endX - startX) / 2;
+  x = Math.abs(endX - startX) / 2
   return {'x': addX + x, 'y': y}
 }
 
 Neilin.Selector.mouseup = function(e){
-  window.endX = e.pageX;
-  window.endY = e.pageY;
+  window.endX = e.pageX
+  window.endY = e.pageY
 
-
-  st = Neilin.Selector.getSelected();
-  st = st.toString();
-  window.st = st;
+  st = Neilin.Selector.getSelected()
+  st = st.toString()
+  window.st = st
 
   if(window.startX + window.startY != window.endX + window.endY && st!='') {
-    xy = computeXY();
-    $(".popup").css({"left": pageXOffset + xy['x']-40, "top": pageYOffset + xy['y']-45});
+    xy = computeXY()
+    $(".popup").css({"left": pageXOffset + xy['x']-40, "top": pageYOffset + xy['y']-45})
     $(".popup").show().animate({ top: pageYOffset + xy['y']-65}, {duration: 500, easing: 'easeOutElastic'})
 
   } else {
-    $(".popup").hide();
+    $(".popup").hide()
   }
 }
 
 Neilin.Selector.mousedown = function(e){
-  window.startX = e.pageX;
-  window.startY = e.pageY;
+  window.startX = e.pageX
+  window.startY = e.pageY
 }
-
-
-
 
 $(document).ready(function(){
   highlightMenu = '<div class="popup highlight-menu highlight-menu-active">'+
@@ -96,28 +95,28 @@ $(document).ready(function(){
         '</li>'+
       '</ul>'+
     '</div>'+
-  '</div>';
+  '</div>'
 
-  $("body").append(highlightMenu);
-  $(document).bind("mouseup", Neilin.Selector.mouseup);
-  $(document).bind("mousedown", Neilin.Selector.mousedown);
+  $("body").append(highlightMenu)
+  $(document).bind("mouseup", Neilin.Selector.mouseup)
+  $(document).bind("mousedown", Neilin.Selector.mousedown)
 
   $('.btn-highlight-menu').on("click", function(e){
     action = $(this).data('action')
     switch(action) {
       case 'twitter':
-        win = window.open('https://twitter.com/intent/tweet?text=' + formatMessage(window.st) + window.location.href,'','width=200,height=100');
-        win.focus();
-        break;
+        win = window.open(TWITTER_SHARE_URL+ formatMessage(window.st) + window.location.href,'','width=200,height=100')
+        win.focus()
+        break
       case 'facebook':
-        win = window.open('https://www.facebook.com/sharer/sharer.php?u='+window.location.href,'','width=200,height=100');
-        win.elements['input'].value = 'hello';
-        break;
+        win = window.open(FACEBOOK_SHARE_URL+window.location.href,'','width=200,height=100')
+        win.elements['input'].value = 'hello'
+        break
       case 'google':
-        win = window.open('https://plus.google.com/share?url='+window.location.href,'','width=200,height=100');
-        break;
+        win = window.open(GOOGLEPLUS_SHARE_URL+window.location.href,'','width=200,height=100')
+        break
     }
-  });
-});
+  })
+})
 
 
